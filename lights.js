ca535fdd497d159d85e7a02f3c829a7eb9c859db
Sasha Bezverkha <sasha.bezverha@gmail.com>
2016-04-13 11:33:21 +0300

    fix

--- lights.js
+++ lights.js
@@ -1,42 +1,37 @@
-//$(".red").style.backgroundColor = "blue";
-////elem.style.backgroundColor = "red";
-//alert(1111);
-//document.getElementsByClassName("red")[0].style.background = "blue";
-//alert(2222);
-//window.onload = function() {
-//  document.getElementsByClassName("red")[0].style.background = "blue";
-//};
 $(document).ready(function() {
-    //$('.red').css("background-color", "red");
-    //alert(11111)
-    var sec1=10;
-    var sec2=2;
-    var sec3=15;
-   var red= setInterval(function() {
+    sec1=10;
+    sec2=2;
+    sec3=15;
+    red= $('.red');
+    yellow= $('.yellow');
+    green= $('.green');
+    t1=$(".sec1");
+    t2=$(".sec2");
+    t3=$(".sec3");
+    setInterval(function() {
        if (sec1 < 0) {
            if (sec2 < 0) {
-               $('.red').css("background-color", " #ffbcad");
-               $('.yellow').css("background-color", " #fffdcb");
-               $('.green').css("background-color", " #2bcd1e");
-               $(".sec2").html("");
-               $(".sec3").html(sec3);
+               red.css("background-color", " #ffbcad");
+              yellow.css("background-color", " #fffdcb");
+               green.css("background-color", " #2bcd1e");
+               t2.html("");
+               t3.html('<p>'+sec3+'</p>');
                sec3--;
-               if(sec3 < 0){$('.green').css("background-color", " #cfffcc");
-                   $(".sec3").html("");
+               if(sec3 < 0){green.css("background-color", " #cfffcc");
+                   t3.html("");
                    sec1=10;sec2=2;sec3=15;}
                else if(sec3 <3 ){
-                $('.green').fadeOut(500).fadeIn(500);}
+                green.animate({ 'backgroundColor': '#cfffcc' }, "slow"); }
            }
            else {
-           $('.yellow').css("background-color", " yellow");
-           $(".sec1").html("");
-           $(".sec2").html(sec2);
+           yellow.css("background-color", " yellow");
+           t1.html("");
+           t2.html('<p>'+sec2+'</p>');
            sec2--;}
-        //clearInterval(red);
         }
         else {
-            $('.red').css("background-color", "red");
-            $(".sec1").html(sec1);
+            red.css("background-color", "red");
+            t1.html('<p>'+sec1+'</p>');
             sec1--;}
     }, 1000);
 });
